c68f5efd422451d69f866ab6861f48180012f8a8
kamran6433 <kamran_12@icloud.com>
2022-10-13 12:52:36 +0100

    Implemented a user registration form.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import secrets
 from flask import Flask
 from main.views import main_blueprint
 from blog.views import blog_blueprint
 from users.views import users_blueprint
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 app.register_blueprint(main_blueprint)
 app.register_blueprint(blog_blueprint)
@@ -12,5 +14,3 @@ app.register_blueprint(users_blueprint)
 
 if __name__ == '__main__':
     app.run()
-
-
